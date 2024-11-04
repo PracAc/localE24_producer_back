b66652f78e84e7701f83e2b4fd330487731930e1
MinJae Kim <skwze@naver.com>
2024-11-04 09:38:58 +0900

    Initial commit

---
+++ src/main/java/org/oz/locale24_producer_back/LocalE24ProducerBackApplication.java
@@ -0,0 +1,13 @@
+package org.oz.locale24_producer_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalE24ProducerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalE24ProducerBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/oz/locale24_producer_back/LocalE24ProducerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oz.locale24_producer_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalE24ProducerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
